ca4c499d19f13136721227e8c0990b352c7575d4
mnkstar <103582688+mnkstar@users.noreply.github.com>
2023-12-01 18:13:44 +0530

    changes done

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,15 +5,15 @@ import CardItem from './CardItem';
 function Cards() {
   return (
     <div className='cards'>
-      <h1>Checkout my amazing work in Graphics Design!</h1>
+      <h1>Checkout my amazing work in Graphics Design and Web Development!</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-9.jpg'
-              text='Illustrations mde using Adobe Illustrator'
-              label='Graphics'
-              path='/products'
+              src='images/img-13.jpg'
+              text='MERN_STACK APP with tailwind Css Project'
+              label='Web Development'
+              path='https://mern-getestate.onrender.com'
             />
             <CardItem
               src='images/img-2.jpg'
